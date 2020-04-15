9f116328e0d5269f99437a8a7901040f67e5480a
Elham Samiei <ESamiei@outlook.com>
2020-04-15 12:54:46 +0430

    added

---
+++ PractiseAlgorithm/DuplicateDataInArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace PractiseAlgorithms
+{
+    public static class DuplicateDataInArray
+    {
+
+        public static bool ContainDuplicates(params int[] sampleData) {
+
+            Dictionary<int, int> data = new Dictionary<int, int>();
+            foreach (int i in sampleData)
+            {
+                if (data.ContainsKey(i))
+                {
+                    return true;
+                }
+                else
+                {
+                    data.Add(i, 1);
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ PractiseAlgorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PractiseAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Practice 1 :this Array has Duplicate Data");
+            int[] sampleArray = WriteSampleArray();
+            ExecuteDuplicateDataInArray(sampleArray);
+            Console.ReadLine();
+
+        }
+
+        private static int[] WriteSampleArray()
+        {
+            Console.Write("Input 8 elements in the array :\n");
+            int[] arr = new int[8];
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Write("\n");
+            return arr;
+        }
+
+        public static void ExecuteDuplicateDataInArray(int[] testArray)
+        {
+            Console.WriteLine(DuplicateDataInArray.ContainDuplicates(testArray));
+            Console.Write("\n");
+        }
+    }
+}
